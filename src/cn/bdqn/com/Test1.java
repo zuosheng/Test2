f0afc04856e9f7f887a38f81df847a9330f9b18d
zuosheng <3268657155@qq.com>
2017-05-27 14:19:25 +0800

    Initial commit

---
+++ src/cn/bdqn/com/Test1.java
@@ -0,0 +1,10 @@
+package cn.bdqn.com;
+
+/**
+ * Created by 故人诉璃歌 on 2017/5/27.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
